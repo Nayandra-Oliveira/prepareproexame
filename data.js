4203bd2b610599003f7c636aa8c67f743c81dfbe
Nayandra-Oliveira <narapeoli@gmail.com>
2025-11-25 22:24:26 -0300

    Update data.js

--- data.js
+++ data.js
@@ -258,7 +258,7 @@ export const DATA = {
       tags: ["laxante", "preparo"],
       blocks: {
         prep:
-          "Hidrate-se bem (250–500 mL de água) nas 1–2 horas anteriores. Evite refeições muito gordurosas no período anterior ao uso, pois podem aumentar náuseas e desconforto.Contraindicações importantes (segundo bula): insuficiência renal, insuficiência cardíaca congestiva, megacólon, náuseas/vômitos intensos, desidratação, dor abdominal sem causa definida.",
+          "Hidrate-se bem (250–500 mL de água) nas 1–2 horas anteriores.",
         during:
           "Pode causar cólicas e evacuação rápida.",
         after:
